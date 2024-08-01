a6c1669cbedf28fbacecbff3fbf4ffdb0b0cc0fa
Brandon <107083173+brandonicardi@users.noreply.github.com>
2024-08-01 12:07:10 -0300

    Delete drizzle.config.js

--- drizzle.config.js
+++
@@ -1,9 +0,0 @@
-
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: './utils/schema.jsx',
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://smartbudgetdb_owner:A8kEzQUIw0cV@ep-jolly-snow-a5aiy38g.us-east-2.aws.neon.tech/smartbudgetdb?sslmode=require',
-    }
-  };
